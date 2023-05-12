d03861d2c51fc02afb64ebeee3709ca9f8decaff
rootVSA <rootvsa@ROOTVSA.com>
2023-05-12 09:49:05 -0400

    Ajustando script de install

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-mkdir $HOME/.vim $HOME/.vim/bundle
-cp -r nerdtree $HOME/.vim/bundle
-cp -r vim-airline $HOME/.vim/bundle
-cp -r vim-airline-themes $HOME/.vim/bundle
+
+VIM="$HOME/.vim"
+BUNDLE="$HOME/.vim/bundle"
+
+if [ ! -d "$HOME/.vim" ]; then
+    mkdir $VIM
+fi
+
+if [ ! -d "$HOME/.vim/bundle" ]; then
+    mkdir $BUNDLE
+fi
+
+PLUGINS=("nerdtree" "vim-airline" "vim-airline-themes")
+
+for i in "$PLUGINS[i]"
+do
+    cp -r $PLUGINS $BUNDLE
+done
+
 cp .vimrc $HOME

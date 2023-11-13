c18c2f510b642c190c68770d5be7d9c8fe477709
ViniAman01 <vinisaman@gmail.com>
2023-11-13 17:05:30 -0400

    Ajustando uninstall.sh

--- vimcfg/uninstall.sh
+++ vimcfg/uninstall.sh
@@ -6,8 +6,7 @@ PLUGINS=(nerdtree vim-airline vim-airline-themes)
 
 for p in ${PLUGINS[@]}
 do
-    rm -r $BUNDLE/$p
+    sudo rm -r $BUNDLE/$p
 done
 
-rm $HOME/.vimrc
-history -c
+sudo rm $HOME/.vimrc
